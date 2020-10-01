0cd0e8be0558d41df31ca7f4abd491861c707927
Aubrey Portwood <code@aubreypwd.com>
2020-10-01 15:47:11 -0600

    Require open too.

--- zsh-plugin-download.plugin.zsh
+++ zsh-plugin-download.plugin.zsh
@@ -4,13 +4,11 @@ if [[ $(command -v antigen) ]]; then
 	antigen bundle aubreypwd/zsh-plugin-require@1.0.1
 	antigen apply
 
-	require "aria2c" "brew reinstall aria2" "brew"
+	require "aria2c" "brew reinstall aria2" "brew" # Automatically install aria2 using homebrew.
 fi
 
-if ! [[ -x $(command -v aria2c) ]]; then
-	echo "Please install aria2 to use download."
-	return;
-fi
+if ! [[ -x $(command -v aria2c) ]]; then >&2 echo "Please install aria2 to use download." && return; fi
+if ! [[ -x $(command -v open) ]];   then >&2 echo "download is for macOS only." && return; fi
 
 ###
  # Wrapper or aria2c
@@ -21,10 +19,15 @@ fi
  # @since 1.0.0
  ##
 function download {
+	if [[ "" = "$1" ]]; then
+		echo "Please supply a URL."
+		return
+	fi
+
+	local connections="$2"
+
 	if [ ""="$2" ]; then
-		connections="16"
-	else
-		connections="$2"
+		local connections="16"
 	fi
 
 	aria2c -x "$connections" "$1" && open ./

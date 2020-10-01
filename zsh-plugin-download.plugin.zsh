926f5136a81659681266b67e97bec4bd64bdc91a
Aubrey Portwood <code@aubreypwd.com>
2020-10-01 15:22:51 -0600

    Only do the antigen require method if antigen is installed, and still check for the required commands in cause we couldn't install them.

--- zsh-plugin-download.plugin.zsh
+++ zsh-plugin-download.plugin.zsh
@@ -1,16 +1,31 @@
 #!/bin/zsh
 
-if [[ -x $(command -v brew) ]] && [[ ! -x $(command -v <brewcommand>) ]]; then
-	brew reinstall <brewcommand>
+if [[ $(command -v antigen) ]]; then
+	antigen bundle aubreypwd/zsh-plugin-require@1.0.1
+	antigen apply
+
+	require "aria2c" "brew reinstall aria2" "brew"
 fi
 
-if ! [[ -x $(command -v <brewcommand>) ]]; then
-	echo "Please install <brewcommand>:";
-	echo "\tHomebrew: brew install <brewcommand>"
+if ! [[ -x $(command -v aria2c) ]]; then
+	echo "Please install aria2 to use download."
+	return;
 fi
 
 ###
- # NAME
+ # Wrapper or aria2c
  #
+ # E.g: download ["http://..."] [How many connections]
+ #
+ # @since Tuesday, May 21, 2019
  # @since 1.0.0
  ##
+function download {
+	if [ ""="$2" ]; then
+		connections="16"
+	else
+		connections="$2"
+	fi
+
+	aria2c -x "$connections" "$1" && open ./
+}
